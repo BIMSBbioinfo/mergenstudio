253905bcf3a7e9233a61ba605912d410601a9932
Artur-man <artur-man@hotmail.com>
2023-11-30 23:27:06 +0100

    delete redundant files

--- R/=.R
+++
@@ -1,6 +0,0 @@
-
-tryCatch({
-  agent <- mergen::setupAgent(name="replicate", model = "llama-2-70b-chat", ai_api_key = Sys.getenv("my_replicate_key"))
-}, error = function(x){
-  response = "Request Failed: check your API configurations"
-})
