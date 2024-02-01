c5bdda5b44a2f0cf69336b31f8b4d316116cf07c
jacqueline-98 <jacqueline-98@hotmail.com>
2024-02-01 13:55:21 +0100

    new exec location

--- inst/assets/js/copyToClipboard.js
+++ inst/assets/js/copyToClipboard.js
@@ -27,19 +27,30 @@ $(document).on('click', '.btn-clipboard', function(event) {
 });
 
 
-
-
 $(document).on('click', '.btn-exec', function(event) {
 
-  // get the parent div of the parent div and next pre tag
-  const parentDiv2 = $(this).closest('div').parent();
-  const preTag2 = parentDiv2.next('pre');
+  // get the closest child div with class "assistant-message-wrapper"
+  const parentDiv = $(this).closest('div').parent();
+  const closestAssistantDiv = parentDiv.next('div.assistant-message-wrapper');
 
-  // find code inside pre tag
-  const codeTag2 = preTag2.find('code');
-  const code2 = codeTag2.text();
+  const textinside = closestAssistantDiv[0].innerText.trim();
 
-  Shiny.setInputValue("app-chat-code", code2,{priority: "event"});
+  // set the value for Shiny input
+  Shiny.setInputValue("app-chat-response", textinside, { priority: "event" });
+
+  // Search for pre tags with a copy button within the closest assistant div
+  // copy button indicates that it is actual code and not some output
+  // which we would not want to run
+  const preTagsWithCopyButton = closestAssistantDiv.find('pre.hasCopyButton');
+
+  //get all code
+  const code = preTagsWithCopyButton.map(function() {
+    const codeTagText = $(this).find('code').text().trim();
+    return codeTagText;
+  }).get();
+
+  // set the value for Shiny input
+  Shiny.setInputValue("app-chat-code", code, { priority: "event" });
 
   // update clipboard button text
   const codeButton = $(this);
@@ -54,10 +65,12 @@ $(document).on('click', '.btn-exec', function(event) {
 
 
 
+
+
 // gpt-created
 function addCopyBtn() {
   // Get all the pre tags in the document that don't already have a copy button
-  var preTags = $('pre:not(".hasCopyButton")');
+  var preTags = $('pre:not(.hasCopyButton):not(.DoesntNeedCopy)');
 
   // Loop through all the pre tags
   preTags.each(function() {
@@ -80,19 +93,44 @@ function addCopyBtn() {
         <button type="button" class="btn action-button btn-secondary btn-sm btn-clipboard shiny-bound-input ml-auto">
             <i class="fas fa-copy"></i> Copy
         </button>
-        <button type="button" class="btn action-button btn-secondary btn-sm btn-exec shiny-bound-input ml-auto">
-        <i></i> Execute
-        </button>
     </div>
     </div>
     `);
-
     // Insert the div with the copy button and language text before the pre tag
     $(this).before(div);
   });
-
 }
 
+function addExecButton() {
+  var AssistantDivs = $('div.assistant-message-wrapper:not(.hasExecButton)');
+
+  AssistantDivs.each(function() {
+    // Check if the current AssistantDiv contains a <code> tag
+    if ($(this).find('pre.hasCopyButton').length > 0) {
+      $(this).addClass('hasExecButton');
+      var div = $(`
+        <div class="d-flex justify-content-between bg-dark" style="border-radius: 5px 5px 0 0">
+          <div>
+            <button type="button" class="btn action-button btn-secondary btn-sm btn-exec shiny-bound-input ml-auto">
+              <i></i> Execute response
+            </button>
+          </div>
+        </div>
+      `);
+
+      // Insert the div with the execute button before the current div
+      $(this).before(div);
+    }
+  });
+}
+
+
+
+
 $(document).on('shiny:inputchanged', function(event) {
+  // DO NOT CHANGE THE ORDER OF THIS! FIRST COPY button
+  // Then exec button so the execbutton can see where there are
+  // answers with actual code chunks in it and not output stuff.
   addCopyBtn();
+  addExecButton();
 });
